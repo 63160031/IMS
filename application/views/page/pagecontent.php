3ea686b35753545531803c5d26182f7ef196236d
63160031 <63160031@go.buu.ac.th>
2024-01-05 19:15:48 +0700

    day1

--- application/views/page/pagecontent.php
+++ application/views/page/pagecontent.php
@@ -14,7 +14,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>SYS_IMS</title>
+    <title>SYS IMS</title>
 
     <meta name="description" content="" />
 
